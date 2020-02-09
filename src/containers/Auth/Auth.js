23d90849c3f46dc670dd802c0f5c9f90eac3f57a
Wei Wei <weiwei.alex@gmail.com>
2020-02-09 16:01:16 +0100

    Add hander trigger

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -112,7 +112,7 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                    {form}
                    <Button btnType="Success">SUBMIT</Button>
                 </form>
