68bbb42ad699b80e78ebdb8d3c19f195a19ef293
Wei Wei <weiwei.alex@gmail.com>
2020-02-09 17:42:34 +0100

    Add signin feature

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
     }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true,
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAp_6VuPBCSYTdEljnXQIFvgyWaSad_JDc', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAp_6VuPBCSYTdEljnXQIFvgyWaSad_JDc'
+        if (!isSignup) {
+            url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAp_6VuPBCSYTdEljnXQIFvgyWaSad_JDc"
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data));
